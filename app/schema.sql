00d4433d887b956f25d5ff36ddd65cd8398cdf14
KENYS GUERRERO ANAYA <kpguerrero@mail.uniatlantico.edu.co>
2022-10-07 22:47:01 -0500

    corrección contraseña de email

--- app/schema.sql
+++ app/schema.sql
@@ -52,4 +52,4 @@ CREATE TABLE credentials(
   password TEXT NOT NULL
 );
 
-INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','causeche@uninorte.edu.co', 'Carlos6842');
+INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','causeche@uninorte.edu.co', 'xxxxxxxxx');
